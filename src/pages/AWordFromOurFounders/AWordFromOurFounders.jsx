dd67c9be3d0b3a67bc72ec7f916d9cd0dfd4fe34
Anthony Daniel <anthonymmarinbolivar@gmail.com>
2024-02-01 13:48:34 -0600

    Added component Icon with text

--- src/pages/AWordFromOurFounders/AWordFromOurFounders.jsx
+++ src/pages/AWordFromOurFounders/AWordFromOurFounders.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Page from "../../components/Page/Page";
+import IconWithText from "../../components/IconWithText/IconWithText";
+import Ship from "/assets/images/ship.svg";
+import Pin from "/assets/images/pin.svg";
 
 function AWordFromOurFounders() {
   return (
@@ -7,6 +10,9 @@ function AWordFromOurFounders() {
       title={<>A Word From Our <span className="big_text">Founders</span></>}
       header_image="/assets/images/a_word_from_our_founders/Header.webp"
     >
+      <IconWithText className={'CTA'} icon={Pin} text={"San Juan, Puerto Rico"} opacity={1}></IconWithText>
+      <IconWithText className={'CTA'} icon={Ship} numbers={34100} text={"sq km mapped "} opacity={0.9}></IconWithText>
+      <IconWithText className={'CTA'} icon={Ship} text={"Without numbers"} opacity={1}></IconWithText>
       <h2>Content</h2>
     </Page>
   );
