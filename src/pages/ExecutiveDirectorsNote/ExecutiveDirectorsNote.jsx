17b52f2bda2906fcff875ca9515f63e68a2bcb34
Anthony Daniel <anthonymmarinbolivar@gmail.com>
2024-02-14 15:27:07 -0600

    Basic structure of the page executive-directors-note

--- src/pages/ExecutiveDirectorsNote/ExecutiveDirectorsNote.jsx
+++ src/pages/ExecutiveDirectorsNote/ExecutiveDirectorsNote.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Page from "../../components/Page/Page";
+import { PageContainer } from "./ExecutiveDirectorsNote.styles";
 
 function ExecutiveDirectorsNote() {
   return (
-    <div>
-      <h2>Executive Director's Note</h2>
-    </div>
+    <PageContainer>
+      <Page
+        title={
+          <img
+            src="/assets/images/executive_directors_note/Title.svg"
+            alt="Executive Directors Note"
+          />
+        }
+        header_image="/assets/images/executive_directors_note/Header.webp"
+        footer_image="/assets/images/executive_directors_note/Footer.webp"
+      >
+      <div className="intro-text">
+        <p>
+          This was a truly historic and momentous year at Schmidt Ocean<br></br>Institute as we embarked on new adventures of scientific<br></br>discovery with R/V Falkor (too).
+        </p>
+      </div>
+      </Page>
+    </PageContainer>
   );
 }
 
---
+++ src/pages/ExecutiveDirectorsNote/ExecutiveDirectorsNote.styles.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  background-image: url("/assets/images/executive_directors_note/Background.webp");
+  background-position: center 50%;
+  background-size: cover;
+  @media (min-width: 830px) {
+    background-position: 50% 45vh;
+    row-gap: 0;
+  }
+  .content {
+    display: grid;
+    grid-template-rows: repeat(50, auto);
+    padding: 8vw 0;
+    @media (min-width: 830px) {
+      grid-template-columns: repeat(2, auto);
+      row-gap: 0;
+    }
+  }
+
+  .intro-text {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .intro-text p {
+    color: #3e5973;
+    font-size: 1.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    max-width: 70%;
+    text-align: center;
+
+    @media (min-width: 830px) {
+      max-width: 60%;
+      font-size: 2.02vw;
+    }
+  }
+`;
