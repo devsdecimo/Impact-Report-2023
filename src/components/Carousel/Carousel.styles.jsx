68b8b03bb77fccc1abb65ffbaa2d66d2f984b370
Anthony Daniel <anthonymmarinbolivar@gmail.com>
2024-02-13 19:27:15 -0600

    Adjusting carousel to make it look like the layout and be flexible

--- src/components/Carousel/Carousel.styles.jsx
+++ src/components/Carousel/Carousel.styles.jsx
@@ -6,7 +6,35 @@ export const CarouselContainer = styled.div`
   align-items: center;
   overflow: hidden;
   position: relative;
-  height: 25em;
+  height: 30em;
+
+  @media (min-width: 0px) {
+    height: 10em;
+  }
+
+  @media (min-width: 360px) {
+    height: 14em;
+  }
+
+  @media (min-width: 515px) {
+    height: 18em;
+  }
+
+  @media (min-width: 768px) {
+    height: 27.5em;
+  }
+
+  @media (min-width: 992px) {
+    height: 28em;
+  }
+
+  @media (min-width: 1600px) {
+    height: 30em;
+  }
+
+  @media (min-width: 1700px) {
+    height: 31em;
+  }
 `;
 
 export const Slide = styled.div`
@@ -15,24 +43,52 @@ export const Slide = styled.div`
   position: absolute;
   left: 50%;
   transform: translateX(-50%);
-  opacity: 0.5;
+  opacity: 0.7;
+
+  @media (max-width: 767px) {
+    min-width: 70%;
+  }
+
   &.center {
     z-index: 2;
     opacity: 1;
-    transform: translateX(-50%) scale(1);
+    transform: translateX(-50%) scale(1.2);
   }
-  &.prev,
-  &.next {
+
+  &.prev {
     z-index: 1;
     opacity: 0.5;
-    transform: translateX(calc(-50% + 33.333%)) scale(0.8);
+    transform: translateX(calc(-50% + 40.333%)) scale(0.8);
   }
+
+  &.next {
+    z-index: 1;
+    opacity: 0;
+    transform: translateX(calc(-50% + 40.333%)) scale(0.8);
+  }
+
   &.prev {
-    transform: translateX(calc(-50% - 33.333%)) scale(0.8);
+    transform: translateX(calc(-50% - 40.333%)) scale(0.8);
   }
+
+  &.first-next  {
+    opacity: 0.5;
+  }
+
+  &.center + &.next {
+    opacity: 0.5;
+  }
+
+  &.first-next, &.prev, &.next {
+    @media (max-width: 767px) {
+      opacity: 0.02;
+    }
+  }
+
   img {
     width: 100%;
     display: block;
+    cursor: grab;
   }
 `;
 
@@ -42,6 +98,25 @@ export const DotContainer = styled.div`
   position: absolute;
   bottom: 10px;
   width: 100%;
+
+  @media (min-width: 280px) {
+    bottom: 29%;
+  }
+  @media (min-width: 360px) {
+    bottom: 22%;
+  }
+  @media (min-width: 515px) {
+    bottom: 14%;
+  }
+  @media (min-width: 768px), (min-width: 1600px) {
+    bottom: 2em;
+  }
+  @media (min-width: 992px) {
+    bottom: 0;
+  }
+  @media (min-width: 1700px) {
+    bottom: 0;
+  }
 `;
 
 export const Dot = styled.span`
@@ -49,8 +124,51 @@ export const Dot = styled.span`
   margin-right: 5px;
   cursor: pointer;
   border-radius: 50%;
-  background: ${(props) => (props.active ? 'black' : 'white')};
+  width: ${(props) => (props.actived ? '10px' : '5px')};
+  height: ${(props) => (props.actived ? '10px' : '5px')};
+  background: ${(props) => (props.actived ? '#5D808F' : 'white')};
+  transition: width 0.3s ease, height 0.3s ease;
   &:last-child {
     margin-right: 0;
   }
 `;
+
+export const ArrowButton = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 3em;
+  color: #5D808F;
+  z-index: 99;
+
+  ${({ direction }) => (direction === 'left' ? 'left: 1%;' : 'right: 1%;')}
+  padding: 10px;
+
+  @media (min-width: 515px) {
+    ${({ direction }) => (direction === 'left' ? 'left: 0;' : 'right: 0;')}
+  }
+
+  @media (min-width: 768px) {
+    ${({ direction }) => (direction === 'left' ? 'left: calc(5% + 10px);' : 'right: calc(5% + 10px);')}
+  }
+
+  @media (min-width: 992px) {
+    ${({ direction }) => (direction === 'left' ? 'left: calc(5% + 20px);' : 'right: calc(5% + 20px);')}
+  }
+
+  @media (min-width: 1200px) {
+    ${({ direction }) => (direction === 'left' ? 'left: calc(10% + 20px);' : 'right: calc(10% + 20px);')}
+  }
+
+  @media (min-width: 1600px) {
+    ${({ direction }) => (direction === 'left' ? 'left: calc(20% + 20px);' : 'right: calc(20% + 20px);')}
+  }
+
+  &:hover {
+    color: #344349;
+  }
+`;
+
--- src/pages/AWordFromOurFounders/AWordFromOurFounders.jsx
+++ src/pages/AWordFromOurFounders/AWordFromOurFounders.jsx
@@ -3,8 +3,23 @@ import Page from "../../components/Page/Page";
 import IconWithText from "../../components/IconWithText/IconWithText";
 import Ship from "/assets/images/ship.svg";
 import Pin from "/assets/images/pin.svg";
+import Carousel from "../../components/Carousel/Carousel";
+import image from "/assets/images/home/expedition_1.webp";
+import image2 from "/assets/images/home/expedition_2.webp";
+import image3 from "/assets/images/home/expedition_3.webp";
+import image4 from "/assets/images/home/expedition_4.webp";
+import image5 from "/assets/images/home/OceanTile.png";
 
 function AWordFromOurFounders() {
+
+  const slides = [
+    { img: image, href: '#', target: '_blank' },
+    { img: image2, href: '#', target: '_blank' },
+    { img: image3, href: '#', target: '_blank' },
+    { img: image4, href: '#' },
+    { img: image5 }
+  ];
+
   return (
     <Page
       title={<>A Word From Our <span className="big_text">Founders</span></>}
@@ -14,6 +29,9 @@ function AWordFromOurFounders() {
       <IconWithText className={'CTA'} icon={Ship} numbers={34100} text={"sq km mapped "} opacity={0.9}></IconWithText>
       <IconWithText className={'CTA'} icon={Ship} text={"Without numbers"} opacity={1}></IconWithText>
       <h2>Content</h2>
+
+      <Carousel slides={slides}></Carousel>
+      <div style={{height: '20em'}}></div>
     </Page>
   );
 }
