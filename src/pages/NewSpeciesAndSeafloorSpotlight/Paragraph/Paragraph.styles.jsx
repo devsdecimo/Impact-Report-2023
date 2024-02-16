9b5366bdf230028a0a04bb3599335df38ae232af
marciacastillo <marcia.castillo@decimoinc.com>
2024-02-16 15:43:28 -0300

    page species y modificacion de componente iconwithtext

---
+++ src/pages/NewSpeciesAndSeafloorSpotlight/Paragraph/Paragraph.styles.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const ParagraphContainer = styled.div`
+  
+  h2, h3, p{
+    font-size:1.5rem;
+  }
+  h2{
+    color:#395169;
+    font-size:1.5rem;
+    font-style:italic;
+    margin-bottom:0;
+    @media (max-width:829px){
+        text-align:center;
+    }
+  }
+  h3{
+    margin-top:10px;
+    @media (max-width:829px){
+        text-align:center;
+    }
+  }
+  p{
+    line-height: 3rem;
+  }
+  p.text{
+    margin-bottom:3rem;
+  }
+  .ml-10{
+    margin-left:5px;
+  }
+
+`;
---
+++ src/pages/NewSpeciesAndSeafloorSpotlight/Section/Section.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { SectionContainer } from "./Section.styles";
+import IconWithText from "../../../components/IconWithText/IconWithText";
+import Depth from "/assets/images/new_species/icons/Depth.svg";
+import Year from "/assets/images/new_species/icons/Year.svg";
+import Location from "/assets/images/new_species/icons/Location.svg";
+import Size from "/assets/images/new_species/icons/Size.svg";
+import Paragraph from "../Paragraph/Paragraph";
+import ImageWithCircle from "../ImageWithCircle/ImageWithCircle";
+
+
+function Section({title, subtitle, text, italicWords, namedBy, vessel, vesselAfterText, photo, depth, depth2, depthSeparator, year, year2, yearSeparator, location,size, sizeText, image, alt}) {
+  return (
+    <SectionContainer>
+        <section className="d-flex">
+          <div className="images-container">
+              <ImageWithCircle image={image} alt={alt}/>
+              <div className="icons-container">
+                  <IconWithText className={'CTA'} icon={Depth} numbers={depth} numbers2={depth2} separator={depthSeparator} text={"m"} opacity={1} textBefore="Depth: "></IconWithText>
+                  <IconWithText className={'CTA'} icon={Year} numbers={year} numbers2={year2} separator={yearSeparator} text={""} opacity={1} textBefore="Year discovered: "></IconWithText>
+                  <IconWithText className={'CTA'} icon={Location}  text={location} opacity={1} textBefore="Location: "></IconWithText>
+                  <IconWithText className={'CTA'} icon={Size} numbers={size} text={sizeText} opacity={1} textBefore="Size: "></IconWithText>
+              </div>
+          </div>
+          <div className="paragraph-container">
+              <Paragraph
+                title={title}
+                subtitle={subtitle}
+                text={text}
+                italicWords={italicWords}
+                namedBy={namedBy}
+                vessel={vessel}
+                vesselAfterText={vesselAfterText?vesselAfterText:""}
+                photo={photo}/>
+          </div>
+      </section>
+    </SectionContainer>
+    
+  );
+}
+
+export default Section;
